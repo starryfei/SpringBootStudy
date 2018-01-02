923ea05fd318277c8d949841d5518bfc41962d0b
starryfei <760646630fei@sina.com>
2018-01-02 21:53:20 +0800

    freemarker

--- src/main/java/com/starry/web/DemoApplication.java
+++ src/main/java/com/starry/web/DemoApplication.java
@@ -2,11 +2,9 @@ package com.starry.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-//@ComponentScan(value = {"com.starry.entity", "com.starry.entity.InfoEntity)"})
 //Spring boot 标识
 public class DemoApplication {
 
---
+++ src/main/java/com/starry/web/GreetingController.java
@@ -0,0 +1,28 @@
+package com.starry.web;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private static final String template = "hello,%s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("gretting")
+    @ResponseBody
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template,
+                name));
+    }
+    @RequestMapping("aa")
+    @ResponseBody
+    public String aa(
+            @RequestParam(value = "name", defaultValue = "world") String name) {
+        return name;
+    }
+}
