5d29aa31f1a1ce95ba00ef476a5f4fb21b1fb8a7
starryfei <760646630fei@sina.com>
2018-03-08 14:50:40 +0800

    add alibaba druid

--- src/main/java/com/starry/Main.java
+++ src/main/java/com/starry/Main.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
