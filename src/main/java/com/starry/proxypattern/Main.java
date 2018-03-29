ba0667e796b07c122ad3789043d33b1cdd5af3e8
starryfei <760646630fei@sina.com>
2018-03-29 10:15:36 +0800

    add cglib proxy

--- src/main/java/com/starry/proxypattern/Main.java
+++ src/main/java/com/starry/proxypattern/Main.java
@@ -4,13 +4,19 @@ import java.lang.reflect.Proxy;
 
 public class Main {
     public static void main(String[] args){
-//        静态代理
+     //    静态代理
         SubInterface sub = new ProxyClass(new SubClass());
         sub.execute();
-//  动态代理client
+    //  JDK动态代理client
         SubInterface subInterface = (SubInterface) Proxy.newProxyInstance(Main.class.getClassLoader(),
                 new Class[]{SubInterface.class},new JDKProxySubject(new SubClass()) );
         subInterface.execute();
         subInterface.delete();
+    //   cglib动态代理      
+        CGLibProxy cProxy = new CGLibProxy();
+        //通过动态生成子类的方式创建代理类
+        ServiceImpl service = (ServiceImpl) cProxy.getPrxoy(ServiceImpl.class);
+        service.create();
+        service.query();
     }
 }
