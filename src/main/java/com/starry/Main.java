58a79f546a106c72def6b39df7ff67201e2ad496
starryfei <760646630fei@sina.com>
2018-04-04 18:25:09 +0800

    study spring 定时任务

--- src/main/java/com/starry/Main.java
+++ src/main/java/com/starry/Main.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-
+/**
+ * 
+ * @ClassName  Main   
+ * @Description SpringBoot启动类  
+ * @author yafei.qin 
+ * @date 2018年4月4日 下午6:21:35  
+ *   
+ *  @EnableCaching:缓存注解
+ *  @EnableScheduling：定时任务注解
+ */
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
